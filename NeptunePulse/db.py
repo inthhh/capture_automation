30b54bb4915438d4a52d28b9077b50e62f055dd9
lob <monjk557@gmail.com>
2024-04-11 18:40:42 +0900

    Refactor : DB Class 및 Mode 추가

--- NeptunePulse/db.py
+++ NeptunePulse/db.py
@@ -2,81 +2,50 @@ import psycopg2
 from dotenv import load_dotenv
 from datetime import datetime
 import os
+load_dotenv(verbose=False)
 
 # ====================================
 # Warning : 외부망에서 접근 불가능 합니다.
 # 사내 망(Cato) 으로 접속 부탁드립니다.
 # ====================================
-def db_dev(dev_status):
-    if dev_status != 'dev':
-        load_dotenv(verbose=True)
-        host = os.getenv("DB_HOST")
-        db_name = os.getenv("DB_NAME")
-        user = os.getenv("DB_USER")
-        password = os.getenv("DB_PASSWORD")
-        port = os.getenv("DB_PORT")
 
-        db = psycopg2.connect(host=host, dbname=db_name, user=user, password=password, port=port)
-        cursor = db.cursor()
-
-
-def execute(query, args={}):
-    try:
-        cursor.execute(query, args)
-        db.commit()
-        row = cursor.fetchall()
-        return row
-    except Exception as e:
-        return e
+host = os.getenv("DB_HOST")
+db_name = os.getenv("DB_NAME")
+test_db_name = os.getenv("TEST_DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+port = os.getenv("DB_PORT")
 
 
 
-# insert_query = "INSERT INTO qa_result (location, title) VALUES (%s, %s);"
-# insert_query = "INSERT INTO qa_result (start_time, finish_time, running_time, rhq, subsidiary, site_code, location, title, description, contents, check_result, check_reason) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-# select_query = "SELECT * FROM qa_result;"
-#
-# start_time = datetime.now()
-# print("start time",start_time)
-# finish_time = datetime.now()
-# running_time = finish_time - start_time
-# rhq = "NANE"
-# subsidiary = "UK"
-# site_code = "UK"
-# location = "Key Visual"
-# title = "TITLE"
-# description = "description"
-# contents = "contents"
-# check_result = "PASS"
-# check_reason = "none"
-# # cursor.execute(insert_query, ("test location", "test title"))
-#
-# # result = execute(insert_query, ("test","test"))
-# result = execute(insert_query, (start_time, finish_time, running_time, rhq, subsidiary, site_code, location, title, description, contents, check_result, check_reason))
-# print(result)
-#
 
 
 
-# class Database():
-#     def __init__(self):
-#
-#         self.cursor = self.db.cursor()
-#         print("Database connection established")
-#
-#
-#     def __del__(self):
-#         self.db.close()
-#         self.cursor.close()
-#
-#     def execute(self,query,args={}):
-#         try:
-#             self.cursor.execute(query, args)
-#             row = self.cursor.fetchall()
-#             return row
-#         except Exception as e:
-#             print("Error while executing :",e)
-#
-#
-#     def commit(self):
-#         self.cursor.commit()
+
+
+class Database :
+    def __init__(self, mod_status):
+        self.conn = psycopg2.connect(host=host, dbname=db_name if mod_status == 'live' else test_db_name,user=user,password=password,port=port)
+        self.cursor = self.conn.cursor()
+    def execute(self, query, args={}):
+        try:
+            self.cursor.execute(query, args)
+            row = self.cursor.fetchall()
+            print(row)
+            return row
+        except Exception as e:
+
+            return e
+    def close_connection(self):
+        self.cursor.close()
+
+    def commit_connection(self):
+        self.conn.commit()
+        self.close_connection()
+
+
+    def rollback_connection(self):
+        self.conn.rollback()
+        self.close_connection()
+
 
