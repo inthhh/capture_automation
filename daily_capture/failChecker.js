514917b0f14d85f52026bd24e243cea98c4d7fb0
lob <monjk557@gmail.com>
2024-04-29 11:15:10 +0900

    feat : daily capture latest version

--- daily_capture/failChecker.js
+++ daily_capture/failChecker.js
@@ -1,5 +1,5 @@
 const checkFailImage = async (page, src) =>{
-    const imageHandle = await page.$(`img[data-desktop-src^="${src}"], img[data-desktop-src^="${src}?"], img[src^="${src}"], img[src^="${src}?"]`);
+    const imageHandle = await page.$(`img[data-desktop-src^="${src}?"], img[data-desktop-src^="${src}?"], img[src^="${src}"], img[src^="${src}?"]`);
     if (imageHandle) {
         // Apply border to the image
         await page.evaluate((element) => {
--- daily_capture/mobile.js
+++ daily_capture/mobile.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
 const carouselBreak = require ('./carouselBreak');
+const failChecker = require("./failChecker");
 
 const delay = (time) => {
     return new Promise(function(resolve) {
@@ -15,7 +16,7 @@ const takeScreenshot = async (siteCode) => {
 
     const page = await browser.newPage();
     const url = `https://www.samsung.com/${siteCode}`;
-    await page.setViewport({ width: 360, height: 10000 });
+    await page.setViewport({ width: 1440, height: 10000 });
     await page.goto(url, {waitUntil: 'load'});
 
     // Get the height of the rendered page
@@ -24,7 +25,7 @@ const takeScreenshot = async (siteCode) => {
     await page.setViewport({ width: Math.floor(body.width), height: Math.floor(body.height)});
 
     //Click Cookie popup accept
-    await page.click('.cookie-bar__close')
+    // await page.click('.cookie-bar__close')
 
     await carouselBreak.carouselBreakMobile(page)
 
@@ -32,13 +33,19 @@ const takeScreenshot = async (siteCode) => {
 
     await carouselBreak.eventListenerBreak(page)
 
+    const failedImage= ["images.samsung.com/is/image/samsung/assets/uk/homepage/LT_DT_684x684_TV-PreOrder-S242.jpg"]
+    for (let i = 0; i < failedImage.length; i++){
+        await failChecker.checkFailImage(page,failedImage[i])
+    }
+
+
     const dateNow = moment().format("YYYY-MM-DD_HH-mm-ss")
 
     const fileName = `C:\\Users\\PTK\\Desktop\\CODE\\weekly_capture_v0.0.2\\result\\${siteCode}-${dateNow}-screenshot.png`
 
     await page.screenshot({ path: fileName, fullPage: true});
-    browser.close();
+    // browser.close();
 
 }
 
-takeScreenshot('us');
+takeScreenshot('uk');
