8c41d4b15cd687cba6198bbf230936a4c9bba064
inthhh <glass005533@gmail.com>
2024-05-21 13:56:21 +0900

    add: unused dir

--- ellipsisDetector.js
+++
@@ -1,8 +0,0 @@
-const ellipsisDetector = async (page) =>{
-
-}
-
-
-module.exports = {
-    'ellipsisDetector':ellipsisDetector
-}
--- unused/ellipsis.js
+++ unused/ellipsis.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const moment = require('moment');
 const carouselBreak = require ('./carouselBreak');
 const failChecker = require('./failChecker')
-const { ellipsisDetector } = require('./ellipsisDetector')
+const { ellipsisDetector } = require('./unused/ellipsisDetector')
 const utility = require("./src/utility");
 
 const db = require("./db")
